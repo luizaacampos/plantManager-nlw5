2fc46195853d432211a527cd84207ad7665159c6
Luiza Campos <luiza.almcampos@gmail.com>
2021-05-03 15:12:25 -0300

    aula 2

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,32 +1,49 @@
 import React from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png'
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
+    const navigation = useNavigation()
+
+    function handleStart() {
+        navigation.navigate('UserIdentification')
+    }
+    
     return (
         <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil
-            </Text>
-
-            <Image source={wateringImg} style={styles.image} />
-
-            <Text style={styles.subtitle}>
-                Não esqueça mais de regar suas plantas. Nós lembramos você sempre que precisar.
-            </Text>
-
-            <TouchableOpacity 
-                style={styles.button}
-                activeOpacity={0.7}
-            >
-                <Text style={styles.buttonIcon}>
-                    -
+            <View style={styles.wrapper}>
+                <Text style={styles.title}>
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil
                 </Text>
-            </TouchableOpacity>
+
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image}
+                    resizeMode="contain"
+                />
+
+                <Text style={styles.subtitle}>
+                    Não esqueça mais de regar suas plantas. Nós lembramos você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity 
+                    style={styles.button}
+                    activeOpacity={0.7}
+                    onPress={handleStart}
+                >
+                    <Feather 
+                        name="chevron-right" 
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -34,25 +51,31 @@ export function Welcome() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'space-around',
+        paddingHorizontal: 20,
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34,
     },
     image: {
-        width: 219,
-        height: 213,
+        height: Dimensions.get('window').width * 0.7,
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
         color: colors.heading,
+        fontFamily: fonts.text,
     },
     button: {
         backgroundColor: colors.green,
@@ -65,6 +88,6 @@ const styles = StyleSheet.create({
     },
     buttonIcon: {
         color: colors.white,
-        fontSize: 24,
+        fontSize: 32,
     }
 })
